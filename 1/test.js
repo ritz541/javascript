a50f7769bd8624261a7af909f1c6f9646956aafe
Ritesh Chavan <riteshshivajichavan@gmail.com>
2025-02-21 17:42:19 +0000

    nothing new

---
+++ 1/test.js
@@ -0,0 +1,7 @@
+const fullName = "Ritesh Chavan";
+let myEmail = 'riteshchavan@gmail.com';
+var myPassword = '12345';
+myCity = "Hadapsar";
+let myStatus;
+
+console.table([fullName, myEmail, myPassword, myCity, myStatus]);
