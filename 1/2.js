9db9c5abdc18a8e16b06bf28be941ffd09b9a537
Ritesh Chavan <riteshshivajichavan@gmail.com>
2025-02-23 03:36:56 +0000

    conversion.

---
+++ 1/2.js
@@ -0,0 +1,12 @@
+// let score = "33";
+// console.log("Initial type: ", typeof score);
+
+// let finalScore = Number(score);
+// console.log("Finial type: ", typeof finalScore);
+
+
+let isGood = 0;
+console.log(typeof isGood);
+
+let finalIsGood = Boolean(isGood);
+console.log(finalIsGood);
--- 1/test.js
+++ 1/test.js
@@ -5,3 +5,13 @@ myCity = "Hadapsar";
 let myStatus;
 
 console.table([fullName, myEmail, myPassword, myCity, myStatus]);
+
+
+let myname = "Ritesh"
+let age = 18
+let isLoggedIn = true
+
+console.log(typeof age);
+console.log(typeof(myname));
+
+
